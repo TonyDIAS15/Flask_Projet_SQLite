0a9050fdd4dda27ac14da9d8c8460a3492898957
TonyDIAS15 <tony.dias15@outlook.fr>
2025-03-18 14:17:56 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,16 +5,16 @@ from urllib.request import urlopen
 from werkzeug.utils import secure_filename
 import sqlite3
 
-app = Flask(__name__)
+app = Flask(__name__)                                                                                                                  
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions
 
 # Fonction pour créer une clé "authentifie" dans la session utilisateur
 def est_authentifie():
     return session.get('authentifie')
 
-# Fonction pour vérifier l'authentification utilisateur standard
-def est_utilisateur_authentifie():
-    return session.get('user_authentifie')
+# Fonction pour créer une clé "user_auth" dans la session utilisateur
+def est_user_authentifie():
+    return session.get('user_auth')
 
 @app.route('/')
 def hello_world():
@@ -36,33 +36,34 @@ def authentification():
             return render_template('formulaire_authentification.html', error=True)
     return render_template('formulaire_authentification.html', error=False)
 
-@app.route('/user_authentification', methods=['GET', 'POST'])
-def user_authentification():
+@app.route('/user_auth', methods=['GET', 'POST'])
+def user_auth():
     if request.method == 'POST':
         if request.form['username'] == 'user' and request.form['password'] == '12345':
-            session['user_authentifie'] = True
-            return redirect(url_for('fiche_nom', id=request.form['id']))
+            session['user_auth'] = True
+            return redirect(url_for('fiche_nom'))
         else:
-            return render_template('formulaire_user_authentification.html', error=True)
-    return render_template('formulaire_user_authentification.html', error=False)
+            return render_template('formulaire_authentification_user.html', error=True)
+    return render_template('formulaire_authentification_user.html', error=False)
+
+@app.route('/fiche_nom/', methods=['GET'])
+def fiche_nom():
+    if not est_user_authentifie():
+        return redirect(url_for('user_auth'))
+
+    nom_recherche = request.args.get('nom', '')
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients WHERE nom = ?', (nom_recherche,))
+    data = cursor.fetchall()
+    conn.close()
+    return render_template('read_data.html', data=data)
 
 @app.route('/fiche_client/<int:post_id>')
 def Readfiche(post_id):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM clients WHERE id = ?', (post_id,))
-    data = cursor.fetchall()
-    conn.close()
-    return render_template('read_data.html', data=data)
-
-@app.route('/fiche_nom/<int:post_id>')
-def fiche_nom(id):
-    if not est_utilisateur_authentifie():
-        return redirect(url_for('user_authentification'))
-
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients WHERE id = ?', (id,))
     data = cursor.fetchall()
     conn.close()
     return render_template('read_data.html', data=data)
@@ -84,7 +85,6 @@ def formulaire_client():
 def enregistrer_client():
     nom = request.form['nom']
     prenom = request.form['prenom']
-
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
     cursor.execute('INSERT INTO clients (created, nom, prenom, adresse) VALUES (?, ?, ?, ?)', (1002938, nom, prenom, "ICI"))
